7f890c86a46ea3805a7949802a9c712097918268
AdrianPisco <emchivan2016@gmail.com>
2025-12-29 00:05:24 -0500

    FASE 10.1: integrar UserSession en login

--- src/main/java/pe/edu/utp/ferresys/service/UsuarioService.java
+++ src/main/java/pe/edu/utp/ferresys/service/UsuarioService.java
@@ -14,6 +14,7 @@ import pe.edu.utp.ferresys.service.base.ServiceTransaccional;
 import pe.edu.utp.ferresys.util.PasswordUtils;
 import pe.edu.utp.ferresys.model.Permiso;
 import pe.edu.utp.ferresys.security.SecurityManager;
+import pe.edu.utp.ferresys.session.UserSession;
 
 /*
 ================================================================================
@@ -66,6 +67,8 @@ public class UsuarioService extends ServiceTransaccional {
 		}
 
 		// CASO 4: LOGIN EXITOSO
+		UserSession.setUsuarioActual(usuario);
+
 		registrarAuditoria(usuario.getUsername(), "LOGIN_EXITOSO");
 
 		return usuario;
